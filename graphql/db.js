87b56fc5d59e94bc6674449671f4c2e13122d31d
Ssnnaaiill <newdk3025@gmail.com>
2020-03-09 15:02:54 +0900

    ✨ Add mutations for movie api

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+let movies = [
   {
     id: 0,
-    name: "Haruka",
-    age: 17,
-    height: 158
+    name: "Star Wars - The new one",
+    score: 10
   },
   {
     id: 1,
-    name: "Chihaya",
-    age: 16,
-    height: 162
+    name: "Avengers - The new one",
+    score: 8
   },
   {
     id: 2,
-    name: "Miki",
-    age: 15,
-    height: 161
+    name: "The Godfather I",
+    score: 99
+  },
+  {
+    id: 3,
+    name: "Logan",
+    score: 2
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movie.filter(movie => movie.id === id);
+  return filteredMovies[0];
 };
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+}
